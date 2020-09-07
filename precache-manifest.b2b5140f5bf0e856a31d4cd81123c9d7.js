4c97793216a16d7e24a5d3a918cc6e1c1ec0bea8
Talk2riyaz <qureshiriyaz154@gmail.com>
2020-09-07 18:41:12 +0700

    Updates

---
+++ precache-manifest.b2b5140f5bf0e856a31d4cd81123c9d7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123c1e8518b6ead0b2a3e3666df1b158",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "44aa51ecf0f1a4e468dc",
+    "url": "/shop/static/css/main.444aa3f3.chunk.css"
+  },
+  {
+    "revision": "24a84a4a6ec53a085743",
+    "url": "/shop/static/js/2.548af291.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop/static/js/2.548af291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44aa51ecf0f1a4e468dc",
+    "url": "/shop/static/js/main.2622e9f2.chunk.js"
+  },
+  {
+    "revision": "adff1868876cfd638634",
+    "url": "/shop/static/js/runtime-main.0643cd3d.js"
+  },
+  {
+    "revision": "b821e26e96e067b8128e5265b7a41db1",
+    "url": "/shop/static/media/s10.b821e26e.jpg"
+  },
+  {
+    "revision": "9eb1ab176474a383f008b2db51bd83c3",
+    "url": "/shop/static/media/s2.9eb1ab17.jpg"
+  },
+  {
+    "revision": "4a9716d81b30c2a3996b32e2b464baf3",
+    "url": "/shop/static/media/s4.4a9716d8.jpg"
+  },
+  {
+    "revision": "828a1f9c7b1c8a0cf79cccdcc121bd40",
+    "url": "/shop/static/media/s5.828a1f9c.jpg"
+  },
+  {
+    "revision": "3bc0d78a63179b4460d3b7fe8a021c15",
+    "url": "/shop/static/media/s7.3bc0d78a.jpg"
+  },
+  {
+    "revision": "759ace99f287b32feaec0d6d29216845",
+    "url": "/shop/static/media/s8.759ace99.jpg"
+  },
+  {
+    "revision": "8e09db03959be35198af874e82a7916b",
+    "url": "/shop/static/media/s9.8e09db03.jpg"
+  }
+]);
